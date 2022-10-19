cf8c33252b671d7f3bd353840feb7d626cc4abb5
Ted Benson <ted@steamship.com>
2022-10-19 14:35:29 -0400

    Add GA

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,12 +3,27 @@ import { Layout } from '@/components/Layout'
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <AudioProvider>
       <Layout>
         <Component {...pageProps} />
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                  });
+          `}
+        </Script>
       </Layout>
     </AudioProvider>
   )
