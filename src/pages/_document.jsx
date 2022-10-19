782a22cb51d06969615497afeaa9f4189234d13f
Ted Benson <ted@steamship.com>
2022-10-19 14:43:26 -0400

    Add GA to _document too

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -17,6 +17,20 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                  });
+          `}
+        </Script>
       </body>
     </Html>
   )
